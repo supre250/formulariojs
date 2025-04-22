8aea9a9ca39f0ff1beb933da7b2beffe7865e96d
supre250 <supremix300@gmail.com>
2025-04-21 23:07:52 -0500

    v2 deber promedio de notas

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -28,8 +28,31 @@ function numMayor(){
     
 }
 
-//crear un programa que se ingrese las 5 notas del primer parcial, verificar si el promedio 
-// es mayor o igual a 7 pasa de anio, si el promedio es igual o mayor 3.5 
-// y menor a 7 queda a suspenso y si es menor a 3 pierde el semestre 
-// verificar que las notas ingresadas esten en un rango dde 0 a 10
+// Crear un programa en js donde se ingrese las cinco notas del primer parcial, verificar si el promedio es mayor o igual a 7 pasa de año
+// Si el promedio es mayor o igual a 3.5 y menor a 7 se queda suspenso y si el promedio es menor a 3.5 pierde el semestre
+// Verficar que las notas ingresadas esten en un rango de 0 a 10
+
+function promedioNotas(){
+    var nota1 = Number(document.getElementById("nota1").value);
+    var nota2 = Number(document.getElementById("nota2").value);
+    var nota3 = Number(document.getElementById("nota3").value);
+    var nota4 = Number(document.getElementById("nota4").value);
+    var nota5 = Number(document.getElementById("nota5").value);
+
+    // Verificar que las notas estén en el rango de 0 a 10
+    if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10 || nota4 < 0 || nota4 > 10 || nota5 < 0 || nota5 > 10) {
+        document.writeln("Las notas deben estar en el rango de 0 a 10.")
+        return;
+    }
+    // Calcular el promedio
+    var promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+    // Verificar el estado del estudiante según el promedio
+    if (promedio >= 7) {
+        document.writeln("El estudiante pasa de año con un promedio de: " + promedio);
+    } else if (promedio >= 3.5) {
+        document.writeln("El estudiante se queda en suspenso con un promedio de:" + promedio);
+    } else {
+        document.writeln("El estudiante pierde el semestre con un promedio de:" + promedio)
+    } 
+}
  
